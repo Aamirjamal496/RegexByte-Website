1338ba02e869b64d9998dac942cd786dab52bc25
AAMIR JAMAL <amirjamal496@gmail.com>
2024-09-21 14:29:32 +0500

    adding db file

--- Sql/dashboardregexbyte.sql
+++ Sql/dashboardregexbyte.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 10, 2024 at 05:18 PM
+-- Generation Time: Sep 21, 2024 at 11:28 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `dashboardregexbyte`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blogs`
+--
+
+CREATE TABLE `blogs` (
+  `id` int(10) NOT NULL,
+  `title` varchar(20) NOT NULL,
+  `description` varchar(40) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `image` varchar(250) NOT NULL,
+  `timestamp` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `blogs`
+--
+
+INSERT INTO `blogs` (`id`, `title`, `description`, `user_id`, `image`, `timestamp`) VALUES
+(13, 'Backend', 'jskahmkjwidhakdk', 27, 'bg_banner1.jpg', '2024-09-18'),
+(14, 'UIUX', 'djssajdhwkjwkjsahdas', 27, 'bg_banner2.jpg', '2024-09-18'),
+(15, 'Frontend', 'sm mnmsndman,ams,am', 27, '1725702625_02abd5f24c8a8e1e0c63.jpg', '2024-09-18');
 
 -- --------------------------------------------------------
 
@@ -77,7 +101,7 @@ INSERT INTO `contact_us` (`columns_id`, `firstName`, `lastName`, `email`, `messa
 CREATE TABLE `images` (
   `id` int(10) NOT NULL,
   `idProject` int(10) NOT NULL,
-  `image` varchar(40) NOT NULL
+  `image` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -86,15 +110,10 @@ CREATE TABLE `images` (
 
 INSERT INTO `images` (`id`, `idProject`, `image`) VALUES
 (38, 25, '1725702624_e2c1889a4e285b72a690.jpg'),
-(39, 26, '1725702624_73c94f19879ffae05d90.jpg'),
 (40, 25, '1725702624_23c6e49b5b82d8f7c609.jpg'),
-(41, 26, '1725702624_5aae7ebe5468c8a1846f.jpg'),
 (42, 25, '1725702624_663f3129ac9086b96717.jpg'),
 (43, 25, '1725702625_090916e86e5313354f2d.jpg'),
-(44, 26, '1725702625_4ab1d1065d19a8004416.jpg'),
 (45, 25, '1725702625_02abd5f24c8a8e1e0c63.jpg'),
-(46, 26, '1725702625_bb49238265090b730514.jpg'),
-(47, 26, '1725702625_dbc742de00e088cd5606.jpg'),
 (48, 28, '1725900986_07cf8e57f6fdc962e7bd.jpg'),
 (49, 28, '1725900987_9481528427c4ab2d063f.jpg'),
 (50, 28, '1725900987_1645876fc7e61cd8f37f.jpg'),
@@ -124,7 +143,6 @@ CREATE TABLE `projects` (
 
 INSERT INTO `projects` (`id`, `idCategory`, `projectTitle`, `projectDescription`, `createdAt`) VALUES
 (25, 59, 'Mobile App', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur tempore iusto sed facilis alia', '0000-00-00 00:00:00.000000'),
-(26, 59, 'Mobile App', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur tempore iusto sed facilis alia', '0000-00-00 00:00:00.000000'),
 (28, 61, 'Web Development', 'lmvslscjslcmsmckllalacmkacmksmcksnckldsvnkckanckasnckancjcnaxmkdnskdakdjknkcmkcnckakmkcnkmaifjkfjadk', '0000-00-00 00:00:00.000000'),
 (29, 63, 'Ui UX design', 'adaksjkaaaaadddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd', '0000-00-00 00:00:00.000000');
 
@@ -135,7 +153,7 @@ INSERT INTO `projects` (`id`, `idCategory`, `projectTitle`, `projectDescription`
 --
 
 CREATE TABLE `slider` (
-  `profile` varchar(20) NOT NULL,
+  `profile` varchar(250) NOT NULL,
   `id` int(10) NOT NULL,
   `title` varchar(30) NOT NULL,
   `status` varchar(10) NOT NULL,
@@ -147,8 +165,8 @@ CREATE TABLE `slider` (
 --
 
 INSERT INTO `slider` (`profile`, `id`, `title`, `status`, `description`) VALUES
-('bg_banner1.jpg', 8, 'We Are Team Of Experts', '1', 'Mobile App Software Development Company'),
-('bg_banner2.jpg', 9, 'Welcome To RegexByte', '1', 'Top Rated Software Development Company');
+('1725702624_e2c1889a4e285b72a690_3.jpg', 14, 'We Are Team Of Experts', '1', 'Mobile App Software Development Company'),
+('1725702624_23c6e49b5b82d8f7c609_1.jpg', 15, 'Welcome To RegexByte', '1', 'Top Rated Software Development Company\r\n');
 
 -- --------------------------------------------------------
 
@@ -177,9 +195,35 @@ INSERT INTO `user-table` (`id`, `firstname`, `lastname`, `email`, `Password`, `p
 (19, 'Majid', 'khan', 'm1@gmail', '$2y$10$BijaBi3bGEiue2drIc/htOdfv9yrtnnl0vnDD.fZf4j4JPfa8tpDu', 12121212, 'user.png', 'active', '2021-12-12 00:00:00'),
 (27, ',mw', 'qlldmk', 'Q@gmail', '$2y$10$4al6ioGgjzAL/jS6sgNgeOeGtBP0guL03j.et9IYVthbIpDNJk1BO', 12121212, 'RBlogo.jfif', 'active', '1221-12-12 00:00:00');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_review`
+--
+
+CREATE TABLE `user_review` (
+  `id` int(10) NOT NULL,
+  `user` varchar(20) NOT NULL,
+  `review` varchar(40) NOT NULL,
+  `created_at` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_review`
+--
+
+INSERT INTO `user_review` (`id`, `user`, `review`, `created_at`) VALUES
+(6, 'Majid', 'mzbfbdsdmds,smd,ma,sm,ams,ams,ma,mkdnsmm', '2024-09-18');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `blogs`
+--
+ALTER TABLE `blogs`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `categories`
@@ -221,8 +265,20 @@ ALTER TABLE `user-table`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indexes for table `user_review`
+--
+ALTER TABLE `user_review`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `blogs`
+--
+ALTER TABLE `blogs`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `categories`
@@ -252,13 +308,19 @@ ALTER TABLE `projects`
 -- AUTO_INCREMENT for table `slider`
 --
 ALTER TABLE `slider`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `user-table`
 --
 ALTER TABLE `user-table`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `user_review`
+--
+ALTER TABLE `user_review`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
